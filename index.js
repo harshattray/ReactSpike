c5d7510106c32afa5f246919773bb1d5fea1e09f
harshattray <harsh.attray@gmail.com>
2017-10-17 22:17:56 +0530

    Google passport Stratergy Setup

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
  * @Author: harsha
  * @Date:   2017-10-17T14:44:21+05:30
  * @Last modified by:   harsha
- * @Last modified time: 2017-10-17T17:41:35+05:30
+ * @Last modified time: 2017-10-17T21:50:12+05:30
  */
 
 const express = require('express');
+const passport = require('passport');
+const passportGoogle = require('passport-google-oauth20').Strategy;
 
 const app = express();
+/*Auth factors*/
+passport.use(new passportGoogle());
 
-app.get('/',(req,res) =>{
-  res.send({hi: 'there'});
-});
-
+/*Port definition from heroku*/
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
